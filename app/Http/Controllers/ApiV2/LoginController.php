3f20355648abdd0ea0f6d713b2012b015e61c8e9
Dominic Lee <leemflee@gmail.com>
2018-05-13 23:23:53 +0800

    first commit

---
+++ app/Http/Controllers/ApiV2/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\ApiV2;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle an authentication attempt.
+     *
+     * @param  \Illuminate\Http\Request $request
+     *
+     * @return Response
+     */
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            // Authentication passed...
+            return redirect()->intended('dashboard');
+        }
+    }
+}
---
+++ routes/apiv2.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Http\Request;
+
+// this apiv2.php doesn't require auth.
+// for testing purpose temporarily
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//Route::middleware('auth:api')->get('/test', function(Request $request) {
+//  return 'test';
+//});
+
+
+Route::get('/', function() {
+    return 'API Version 2';
+});
+
+//Route::post('/xxx', function() {
+//    return 'API Version 2 post';
+//});
+
+Route::middleware('auth:api')->get('/user', function( Request $request) {
+    return $request->user();
+});
+
+Route::group(['middleware'=>'auth:api'], function() {
+//    Route::get('user', 'UserController@getUser');
+    Route::get('products/init', 'ProductController@init');
+    Route::resource('products', 'ProductController');
+    Route::resource('meeting_rooms', 'MeetingRoomController');
+    Route::resource('meetings', 'MeetingController');
+    Route::get('users/init', 'UserController@init');
+});
+
+// Route::post('/auth', 'LoginController@authenticate');
+//Route::middleware('auth:api')->group(function () {
+//    Route::get('user', 'UserController@getUser');
+//    Route::get('products/init', 'ProductController@init');
+//    Route::resource('products', 'ProductController');
+//    Route::resource('meeting_rooms', 'MeetingRoomController');
+//    Route::resource('meetings', 'MeetingController');
+//    Route::get('users/init', 'UserController@init');
+//});
+//
